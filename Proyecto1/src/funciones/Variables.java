a8b8d4bdd5125fe7392ce72c370d8225139ae8bb
Mario <mariomarroquin1905@gmail.com>
2024-02-24 08:29:17 -0600

    inicioGraficas

--- Proyecto1/src/funciones/Variables.java
+++ Proyecto1/src/funciones/Variables.java
@@ -6,14 +6,23 @@ import java.util.HashMap;
 public class Variables {
 
     public static HashMap<String, Object> variables = new HashMap<>();
+    public static HashMap<String, Object> graficasValores = new HashMap<>();
 
     public static void addVariable(String nombre, Object valor) {
         variables.put(nombre, valor);
     }
 
     public static Object getVariable(String nombre) {
-        //return (String) variables.get(nombre);
         return variables.get(nombre);
+    }
+    
+    
+    public static void addGrafica(String nombre, Object valor) {
+        graficasValores.put(nombre, valor);
+    }
+
+    public static Object getGrafica(String nombre) {
+        return graficasValores.get(nombre);
     }
 
     
